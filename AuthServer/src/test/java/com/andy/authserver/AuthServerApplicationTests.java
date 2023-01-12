4f954dbc386fee39c5ab5ba366141a3826c97125
gaojie <gaojie221796@ehousechina.com>
2023-01-12 13:50:01 +0800

    first commit

---
+++ AuthServer/src/test/java/com/andy/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
